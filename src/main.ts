d975fda35c65010191f6f8a295b3313231217785
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2024-12-12 09:06:00 +0800

    Cors Added

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*"
+    origin: '*',
+    credentials: true
   });
 
   await app.listen(3000);
