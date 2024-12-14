81c9fe0225db8a5fd18956ba62e5747624b2f842
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2024-12-14 21:52:44 +0800

    Authentication Done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,7 +22,7 @@ import { User } from '@prisma/client';
 import { SignupDto, LoginDto, OtpDto, UploadProfileDto } from './dto/index';
 import { AuthService } from './auth.service';
 import * as jwt from 'jsonwebtoken';
-import MulterOptions from '..multer.config';
+import MulterOptions from '../config/multer.config';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('auth')
