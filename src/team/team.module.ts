d4150d5c67ab371edc1d282cd3461c116cff16ce
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2024-12-21 10:57:30 +0800

    Bug fixed

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule
+  ],
   controllers: [TeamController],
   providers: [TeamService]
 })
