f3e5ce468e0ae165692e8505cba3f3651f3d51dd
AbiMantra132 <103944081+AbiMantra132@users.noreply.github.com>
2024-12-27 20:46:46 +0800

    fixing the routes for notif and logout

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -10,10 +10,44 @@ export class NotificationService {
       const notification = await this.prisma.notification.findMany({
         where: { receiverId: id },
       });
+
+      const receiverUser = await this.prisma.user.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          student_id: true,
+          profile: true,
+        }
+      });
+
+      const senderUsers = await Promise.all(notification.map(async (notif) => {
+        return await this.prisma.user.findUnique({
+          where: { id: notif.senderId },
+          select: {
+          id: true,
+          name: true,
+          student_id: true,
+          profile: true,
+          }
+        });
+      }));
+
+      if (!receiverUser) {
+        throw new Error('Receiver user not found');
+      }
+
       if (!notification) {
         throw new Error('Notification not found');
       }
-      return notification;
+
+      return {
+        receiverUser,
+        notifications: notification.map((notif, index) => ({
+          ...notif,
+          senderUser: senderUsers[index],
+        })),
+      };
     } catch (error) {
       throw new Error(`Failed to get notification: ${error.message}`);
     }
